f6609139259963ab8cc43e415c8d00229de5c407
moffittengine <moffitt.engine@gmail.com>
2017-10-21 20:49:51 -0700

    updated test.pas to include more code

--- cmpe152Hw4/test.pas
+++ cmpe152Hw4/test.pas
@@ -1,19 +1,17 @@
 PROGRAM ComplexBuiltIn;
 
 TYPE
-    mystring = ARRAY[1..3] OF char;
-
-        comp = RECORD
-                  re, im : real
-              END;
-              
+    mystring = ARRAY[1..3] OF char;            
 
 VAR
     x, y, z : complex;
     
-
+PROCEDURE print(expr : mystring; VAR z : complex);
+    BEGIN
+        write(expr, ' = (', z.re:0:5, ', ', z.im:0:5, ') ');
+    END;
 
 BEGIN {ComplexTest}
-    x.re := 3;    x.im := 3; 
-    y.re := 8;    y.im := 8; 
+    x.re := 3; x.im := 2;  print('  x', x);
+    y.re := 8; y.im := -5; print('  y', y);
 END {ComplexTest}.
